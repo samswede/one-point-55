7b4e4b2c9a71e058d9cc70804d77bea995aa300c
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-02-10 10:50:32 +0100

    next config and nextvideo

--- one-point/next.config.mjs
+++ one-point/next.config.mjs
@@ -1,4 +1,4 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
 //const withVideos = require('next-videos')
 import withVideos from 'next-videos'; // i need to somehow configure this
