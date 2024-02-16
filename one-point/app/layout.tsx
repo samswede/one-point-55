c4cf751b7031664e8d691d68e1ec3356827082e5
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-02-16 16:24:14 +0100

    init posthog

--- one-point/app/layout.tsx
+++ one-point/app/layout.tsx
@@ -1,19 +1,20 @@
 import './globals.css';
 import type { Metadata } from 'next';
 
+// Post Hog
+import { PHProvider } from './providers'
+import dynamic from 'next/dynamic'
+
+// TinaCMS
 import client from "@/tina/__generated__/client";
 
+// Fonts
 import { IBM_Plex_Sans } from 'next/font/google';
 
+// Components
 import { NavBarMenu } from '@/components/shared/navbar/NavBarMenu';
 import { FooterDemo } from "@/components/shared/footer/FooterDemo";
 
-
-const ibmPlexSans = IBM_Plex_Sans({
-  display: 'swap',
-  subsets: ['latin'],
-  weight: ['500', '600', '700'],
-});
 
 
 export const metadata: Metadata = {
@@ -21,22 +22,46 @@ export const metadata: Metadata = {
   description: 'Unique Vacation Experiences',
 };
 
+
+// Post Hog
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+  ssr: false,
+})
+
+
+// Fonts
+const ibmPlexSans = IBM_Plex_Sans({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['500', '600', '700'],
+});
+
+
+
+
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Fetch tinaCMS data
   const result = await client.queries.global({ relativePath: "Global.md"})
+
   return (
     <html lang="en">
-      <body className={ibmPlexSans.className}>
-        <header>
-          <NavBarMenu />
-        </header>
-        {children}
-        
-        <FooterDemo />
-      </body>
+      <PHProvider>
+        <body className={ibmPlexSans.className}>
+
+          <PostHogPageView /> 
+
+          <header>
+            <NavBarMenu />
+          </header>
+          {children}
+          
+          <FooterDemo />
+        </body>
+      </PHProvider>
     </html>
   );
 }
