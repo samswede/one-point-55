d5f6c84b38a0562dcbc32f938535a2149bd33d3a
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-03-16 15:59:37 +0100

    fix maybe

--- one-point/components/pages/trips/gallery/TripUpcomingGallerySection.tsx
+++ one-point/components/pages/trips/gallery/TripUpcomingGallerySection.tsx
@@ -4,12 +4,16 @@ import { TripupcomingBlocksGallery } from "@/tina/__generated__/types"
 import { tinaField } from "tinacms/dist/react"
 
 import { Gallery } from "react-grid-gallery";
+import CustomGallery from "@/components/shared/gallery/CustomGallery";
 
 export function GallerySection(props: TripupcomingBlocksGallery) {
   return (
     <section className="relative overflow-hidden">
 
+      {/* 
       <Gallery images={props.items || []} />
+    */}
+      <CustomGallery images={props.items || []} />
       
     </section>
   )
