1e08d1bb2e615ab575661c15bfac0cedc1f1e4ff
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-03-16 18:25:02 +0100

    fixed footer

--- one-point/components/shared/footer/FooterDemo.tsx
+++ one-point/components/shared/footer/FooterDemo.tsx
@@ -22,11 +22,11 @@ export function FooterDemo() {
       "
       >
         <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
-          <Link href={"/"} className="text-2xl text-white">
+          <Link href={"/"} className="text-2xl text-foreground">
             Get our Newsletter
           </Link>
           <p className="mt-2 text-xs text-justify text-gray-400">
-            Exclusive discounts and notifications on new trips experiences.
+            Exclusive discounts and heads up on new trips and experiences.
           </p>
           <div className="flex mt-4">
             <Input type="email" placeholder="Email" />
@@ -39,43 +39,47 @@ export function FooterDemo() {
 
           
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
+            <h2 className="mb-2 font-bold tracking-widest text-foreground">
               Social Media
             </h2>
             <div className="flex justify-center mt-4 space-x-4 lg:mt-2">
               
                 <Link href={""}>
-                <Twitter className="text-sky-300" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-twitter-x" viewBox="0 0 16 16">
+                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                  </svg>
                 </Link>
                 <Link href={""}>
-                <Instagram className="text-pink-500" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-instagram" viewBox="0 0 16 16">
+                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                  </svg>
                 </Link>
               
             </div>
           </div>
 
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
+            <h2 className="mb-2 font-bold tracking-widest text-foreground">
               Home
             </h2>
             <ul className="mb-8 space-y-2 text-sm list-none">
               <li>
-                <Link href={"/"} className="text-gray-300">
+                <Link href={"/"} className="text-primary">
                   Concept
                 </Link>
               </li>
               <li>
-                <Link href={"/"} className="text-gray-300">
+                <Link href={"/"} className="text-primary">
                   Upcoming Trips
                 </Link>
               </li>
               <li>
-                <Link href={"/"} className="text-gray-300">
+                <Link href={"/"} className="text-primary">
                   Recent Trips
                 </Link>
               </li>
               <li>
-                <Link href={"/"} className="text-gray-300">
+                <Link href={"/"} className="text-primary">
                   Testimonials
                 </Link>
               </li>
@@ -83,60 +87,34 @@ export function FooterDemo() {
           </div>
 
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
+            <h2 className="mb-2 font-bold tracking-widest text-foreground">
               About
             </h2>
             <ul className="mb-8 space-y-2 text-sm list-none">
               <li>
-                <Link href={"/"} className="text-gray-300">
-                  Mission
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Team
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
+                <Link href={"/about/#gallery"} className="text-primary">
                   Gallery
                 </Link>
               </li>
               <li>
-                <Link href={"/"} className="text-gray-300">
-                  Itineraries
+                <Link href={"/about/#contact"} className="text-primary">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link href={"/about/#FAQ"} className="text-primary">
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link href={"/about/#legal"} className="text-primary">
+                  Legal
                 </Link>
               </li>
             </ul>
           </div>
 
-          <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Legal Information
-            </h2>
-            <ul className="mb-8 space-y-2 text-sm list-none">
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 1
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 2
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 3
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 4
-                </Link>
-              </li>
-            </ul>
-          </div>
+          
         </div>
       </div>
       <div className="flex justify-center -mt-12">
